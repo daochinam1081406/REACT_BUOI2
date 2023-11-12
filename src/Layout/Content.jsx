513d466cfff709a3defa32b581d6a227b68a276f
daochinam <daochinam1081406@gmail.com>
2023-11-13 00:18:32 +0700

    fix

--- src/Layout/Content.jsx
+++ src/Layout/Content.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import glassesData from "./glassesData.json";
 
-export default function Content() {
+const Content = () => {
+  const [selectedGlass, setSelectedGlass] = useState(null);
+
+  const selectGlass = (glass) => {
+    setSelectedGlass(glass);
+  };
+
   return (
     <div className="content">
-      <div className="inner-content">
-        <div className="portrait-images">
-          <img
-            src={process.env.PUBLIC_URL + "/img/model.jpg"}
-            alt="Portrait 1"
-          />
-          <img
-            src={process.env.PUBLIC_URL + "/img/model.jpg"}
-            alt="Portrait 2"
-          />
-        </div>
-        <div className="glass-item"></div>
+      <div className="model">
+        <img
+          // src={
+          //   selectedGlass
+          //     ? process.env.PUBLIC_URL + selectedGlass.url
+          //     : process.env.PUBLIC_URL + "./img/model.jpg" // Default image
+          // }
+          src={
+            "./img/model.jpg" // Default image
+          }
+          alt={selectedGlass ? selectedGlass.name : "Default model"}
+        />
+        <img
+          // src={
+          //   selectedGlass
+          //     ? process.env.PUBLIC_URL + selectedGlass.url
+          //     : process.env.PUBLIC_URL + "./img/model.jpg" // Default image
+          // }
+          src={
+            "./img/model.jpg" // Default image
+          }
+          alt={selectedGlass ? selectedGlass.name : "Default model"}
+        />
+      </div>
+      <div className="glass-items">
+        {glassesData.map((glass) => (
+          <div key={glass.id} onClick={() => selectGlass(glass)}>
+            <img src={process.env.PUBLIC_URL + glass.url} alt={glass.name} />
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default Content;
