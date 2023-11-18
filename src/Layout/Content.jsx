e3519c9eeee924579d1241eb1c1bc79f29bae7c3
daochinam <daochinam1081406@gmail.com>
2023-11-18 23:57:30 +0700

    agit commit m

--- src/Layout/Content.jsx
+++ src/Layout/Content.jsx
@@ -1,5 +1,8 @@
+// Content.jsx
+
 import React, { useState } from "react";
 import glassesData from "./glassesData.json";
+import { Row, Col } from "react-bootstrap"; // Import Row and Col from Bootstrap
 
 const Content = () => {
   const [selectedGlass, setSelectedGlass] = useState(null);
@@ -10,36 +13,44 @@ const Content = () => {
 
   return (
     <div className="content">
-      <div className="model">
-        <img
-          // src={
-          //   selectedGlass
-          //     ? process.env.PUBLIC_URL + selectedGlass.url
-          //     : process.env.PUBLIC_URL + "./img/model.jpg" // Default image
-          // }
-          src={
-            "./img/model.jpg" // Default image
-          }
-          alt={selectedGlass ? selectedGlass.name : "Default model"}
-        />
-        <img
-          // src={
-          //   selectedGlass
-          //     ? process.env.PUBLIC_URL + selectedGlass.url
-          //     : process.env.PUBLIC_URL + "./img/model.jpg" // Default image
-          // }
-          src={
-            "./img/model.jpg" // Default image
-          }
-          alt={selectedGlass ? selectedGlass.name : "Default model"}
-        />
-      </div>
-      <div className="glass-items">
-        {glassesData.map((glass) => (
-          <div key={glass.id} onClick={() => selectGlass(glass)}>
-            <img src={process.env.PUBLIC_URL + glass.url} alt={glass.name} />
-          </div>
-        ))}
+      <Row>
+        <Col className="col-4">
+          <img
+            className="img-model"
+            src={"./img/model.jpg"}
+            alt={selectedGlass ? selectedGlass.name : "Default model"}
+          />
+        </Col>
+        <Col className="col-4" style={{ position: "relative", zIndex: 1 }}>
+          <img
+            className="img-model"
+            src={"./img/model.jpg"}
+            alt={selectedGlass ? selectedGlass.name : "Default model"}
+          />
+        </Col>
+        <Col className="col-4" style={{ position: "relative", zIndex: 2 }}>
+          <img
+            className="img-model"
+            src={
+              selectedGlass ? process.env.PUBLIC_URL + selectedGlass.url : ""
+            }
+            alt={selectedGlass ? selectedGlass.name : "Default model"}
+          />
+        </Col>
+      </Row>
+      <div className="glass-content">
+        <Row>
+          {glassesData.map((glass) => (
+            <Col key={glass.id} className="col-3">
+              <img
+                src={process.env.PUBLIC_URL + glass.url}
+                alt={glass.name}
+                className="img-fluid"
+                onClick={() => selectGlass(glass)}
+              />
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
